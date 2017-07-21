a39c86a2b61825efaee4e58d1a07a14d1cb6f8d9
boris <borisvasilev395@gmail.com>
2017-07-21 03:49:38 +0300

    Added author details component

--- component-state-lifecycle-exercise/src/Routes.js
+++ component-state-lifecycle-exercise/src/Routes.js
@@ -7,6 +7,7 @@ import HomePage from './components/HomePage'
 import BooksAll from './components/BooksAll'
 import BookDetails from './components/BookDetails'
 import AuthorsAll from './components/AuthorsAll'
+import AuthorDetails from './components/AuthorDetails'
 let Routes = ()=>(
 <Switch>
     <Route path="/books/all" component={BooksAll}/>
